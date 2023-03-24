cfb6de7b3cd9a93666389eefc6dc782d4e4eb6ee
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-03-24 19:57:22 +0100

    :page_facing_up: Update license header in PHP files

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,1 +1,12 @@
 <?php
+
+/*
+ * This file is part of the Eduroo Framework.
+ *
+ * (c) Jakub Tobiasz
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
