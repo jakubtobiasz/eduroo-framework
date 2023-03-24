df64f7131e50550a5475e9c9ca61181f1de3fec2
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-03-24 20:42:10 +0100

    :heavy_plus_sign: Install deptrac

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -12,6 +12,7 @@ return static function (MBConfig $mbConfig): void {
             'captainhook/captainhook' => '^5.16',
             'pestphp/pest' => '^2.2',
             'phpstan/phpstan' => '^1.10',
+            'qossmic/deptrac-shim' => '^1.0',
             'symplify/monorepo-builder' => '11.2.2.72',
             'symplify/easy-coding-standard' => '^11.3',
             'vimeo/psalm' => '^5.8'
