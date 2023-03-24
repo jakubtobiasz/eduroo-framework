6609768ad30d9ce8cecd18d5ec04db566383873b
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-03-24 20:18:37 +0100

    :heavy_plus_sign: Install phpstan

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -9,8 +9,9 @@ return static function (MBConfig $mbConfig): void {
 
     $mbConfig->dataToAppend([
         'require-dev' => [
-            'symplify/monorepo-builder' => '11.2.2.72',
             'pestphp/pest' => '^2.2',
+            'phpstan/phpstan' => '^1.10',
+            'symplify/monorepo-builder' => '11.2.2.72',
             'symplify/easy-coding-standard' => '^11.3',
         ]
     ]);
