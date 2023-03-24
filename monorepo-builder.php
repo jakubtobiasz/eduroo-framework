fb6c104c3f08a5ddd56da5036b4d64dddd90f03e
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-03-24 19:46:24 +0100

    :heavy_plus_sign: Install symplify/monorepo-builder

---
+++ monorepo-builder.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\Config\MBConfig;
+
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->packageDirectories([__DIR__ . '/components']);
+
+    $mbConfig->dataToAppend([
+        'require-dev' => [
+            'symplify/monorepo-builder' => '11.2.2.72'
+        ]
+    ]);
+};
